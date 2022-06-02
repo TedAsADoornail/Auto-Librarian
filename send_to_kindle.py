f71af617e0e3630e3397a95dc99e17027f7015d6
TedAsADoornail <deridder.ted@gmail.com>
2022-06-02 19:39:44 -0400

    TED-11 Fixed file path

--- send_to_kindle.py
+++ send_to_kindle.py
@@ -7,7 +7,7 @@ to = 'teddyd93_Rkc6lB@kindle.com'
 
 def send_book_to_kindle_email(file_name):
     subject = file_name
-    content = [file_name, file_name]
+    content = [file_name, 'books/' + file_name]
 
     with yagmail.SMTP(user, app_password) as yag:
         yag.send(to, subject, content)
